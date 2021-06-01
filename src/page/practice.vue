2162eac692f96741bf9fced9ee36ef02d36f505d
Leon Wu <wgl_19871107@163.com>
2021-06-01 17:43:43 +0800

    update files

---
+++ src/page/practice.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="practice fillcontain">
+       
+        <el-row>
+
+            <p class="text">Leon's first Vue page!</p>
+
+            <img :src="test" class="image">
+
+        </el-row>
+         
+
+    </div>
+</template>
+
+<script>
+
+    import test from '../assets/img/test.jpeg'
+    export default {
+
+        data () {
+            return {
+                test
+            }
+        }
+    	
+    }
+</script>
+
+<style lang="less">
+	@import '../style/mixin';
+    .practice{
+        background: yellow;
+    }
+    .text{
+		
+		text-align: center;
+		font-size: 100px;
+		color:black;
+	}
+    .image{
+        width: 100%;
+        // height: 500pt;
+    }
+</style>
